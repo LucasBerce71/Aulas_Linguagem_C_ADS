1b9eaff7fb6871a561cd0713b72d20a1f28206ed
LucasBerce71 <lucas.berce@apinteractive.com.br>
2020-09-03 21:30:07 -0300

    feat: Adding exercise 2

--- Aula_01/main.c
+++ Aula_01/main.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 
 int main() {
-    printf("1 - Hamburguer R$20,00");
-    printf("2 - Batata Frita R$30,00");
-    printf("3 - Refrigerante R$10,00");
+    //Ex 01 - Start
+    //printf("1 - Hamburguer R$20,00");
+    //printf("2 - Batata Frita R$30,00");
+    //printf("3 - Refrigerante R$10,00");
+    //Ex 01 - End
+
+    //Ex 02 - Start
+    int number;
+    printf("Digite um número inteiro qualquer:\n");
+    scanf("%i", &number);
+    printf("\nO número digitado foi: %i \n", number);
     system("pause");
 }
